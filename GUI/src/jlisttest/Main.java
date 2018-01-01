5613fa7ac07964d55d96aa06f46904e2b7361a01
xczhw <xczhw@outlook.com>
2018-01-01 17:23:39 +0800

    startdoing

--- GUI/src/jlisttest/Main.java
+++ GUI/src/jlisttest/Main.java
@@ -1,5 +1,40 @@
 package jlisttest;
 
-public class Main {
+import java.awt.Dimension;
+import java.awt.event.*;
+import java.io.File;
+import javax.swing.*;
 
+public class Main {
+	public static void main(String[] args)
+	{
+		File top = new File("/Users/xczhw/FileTester");
+		JScrollPane jsp = new JScrollPane();
+		File[] listF = top.listFiles();
+		JList<File> l = new JList<File>(listF);
+		l.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e)
+			{
+				if(e.getClickCount() == 2)
+				{
+					int index = l.locationToIndex(e.getPoint());
+					System.out.println("Double clicked on Item " + index + listF[index]);// + clicked);
+				}
+			}
+		});
+		jsp.setViewportView(l);
+		
+		
+		JPanel panel = new JPanel();
+		panel.add(jsp);
+		
+		JFrame frame = new JFrame("haha");
+		frame.getContentPane().add(panel);
+		frame.setVisible(true);
+		frame.setSize(350, 350);
+	}
+	private class mls extends MouseAdapter
+	{
+		
+	}
 }
