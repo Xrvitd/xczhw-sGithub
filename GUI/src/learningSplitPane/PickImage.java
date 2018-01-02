c33bb02b0ddb88e474fa0edb5b59e86ca6a5a164
xczhw <xczhw@outlook.com>
2018-01-02 21:18:21 +0800

    0.2
    暂时实现了文件夹的切换

--- GUI/src/learningSplitPane/PickImage.java
+++ GUI/src/learningSplitPane/PickImage.java
@@ -6,29 +6,25 @@ import javax.swing.*;
 
 public class PickImage
 {
-   //-----------------------------------------------------------------
-   //  Creates and displays a frame containing a split pane. The
-   //  user selects an image name from the list to be displayed.
-   //-----------------------------------------------------------------
-   public static void main (String[] args)
-   {
-      JFrame frame = new JFrame ("Pick Image");
-      frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-
-      JLabel imageLabel = new JLabel();
-      JPanel imagePanel = new JPanel();
-      imagePanel.add (imageLabel);
-      imagePanel.setBackground (Color.white);
-
-      ListPanel imageList = new ListPanel (imageLabel);
-
-      JSplitPane sp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-                                     imageList, imagePanel);
-
-      sp.setOneTouchExpandable (true);
-
-      frame.getContentPane().add (sp);
-      frame.pack();
-      frame.setVisible(true);
-   }
+	public static void main (String[] args)
+	{
+		JFrame frame = new JFrame ("Pick Image");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		
+		JLabel imageLabel = new JLabel();
+		JPanel imagePanel = new JPanel();
+		imagePanel.add (imageLabel);
+		imagePanel.setBackground (Color.white);
+		
+		ListPanel imageList = new ListPanel (imageLabel);  
+		
+		JSplitPane sp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+										imageList, imagePanel);
+		
+		sp.setOneTouchExpandable (true);
+		
+		frame.getContentPane().add (sp);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
